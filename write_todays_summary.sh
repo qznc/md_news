21e86af762e5b759214b4ee04f5715b137ece7bf
Andreas Zwinkau <zwinkau@mailbox.org>
2026-06-11 00:00:32 +0200

    refactor top-level script

--- write_todays_summary.sh
+++ write_todays_summary.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-# HN Summary
-OUTPUT_DIR="_out/hn/$(date +"%Y")/$(date +"%m")"
-OUTPUT_FILE="$(date +"%d").md"
+summarize() {
+    BASE_DIR_NAME=$1
+    SCRIPT=$2
+    OUTPUT_DIR="./_out/$BASE_DIR_NAME/$(date +"%Y")/$(date +"%m")"
+    OUTPUT_FILE="$(date +"%d").md"
+    TMP_FILE="$OUTPUT_FILE.tmp"
 
-mkdir -p "$OUTPUT_DIR"
-./hn_summary.py > "$OUTPUT_DIR/$OUTPUT_FILE"
-echo "HN Output written to: $OUTPUT_DIR/$OUTPUT_FILE" >&2
+    mkdir -p "$OUTPUT_DIR"
+    "./$SCRIPT" > "$OUTPUT_DIR/$TMP_FILE"
+    mv -f "$OUTPUT_DIR/$TMP_FILE" "$OUTPUT_DIR/$OUTPUT_FILE" # atomic update
+    echo "Output written to: $OUTPUT_DIR/$OUTPUT_FILE" >&2
+}
 
-# AI Summary
-AI_OUTPUT_DIR="_out/ai/$(date +"%Y")/$(date +"%m")"
-AI_OUTPUT_FILE="$(date +"%d").md"
-
-mkdir -p "$AI_OUTPUT_DIR"
-./ai_summary.py > "$AI_OUTPUT_DIR/$AI_OUTPUT_FILE"
-echo "AI Output written to: $AI_OUTPUT_DIR/$AI_OUTPUT_FILE" >&2
-
+summarize "hn" "hn_summary.py"
+summarize "ai" "ai_summary.py"
 ./to_html.py
 ./gen_index.py
